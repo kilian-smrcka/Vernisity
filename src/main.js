c43f32dec9d5d9c4fa2bd44b477b71baa1b501f6
kilian-smrcka <kilian@smrcka.email>
2024-11-04 14:46:13 +0100

    changed to simpler website

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue"; // Main Vue component
-import "./assets/bootstrap.css"; //Geo-styling
 
 createApp(App).mount("#app"); // Mounts the app to the div with id="app"
